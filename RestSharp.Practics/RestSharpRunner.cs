7712a9a57167ebfd2ba10e16dfce2e40a303a786
Ernest Kyei Nkrumah <Mishrule1@gmail.com>
2023-02-05 22:29:12 +0000

    Add project files.

---
+++ RestSharp.Practics/RestSharpRunner.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using NUnit.Framework;
+
+namespace RestSharp.Practics
+{
+    internal class RestSharpRunner
+    {
+        static void Main(string[] args)
+        {
+            var request = new RestRequest();
+            var client = new RestClient("https://www.google.com");
+            Console.WriteLine($"{client.Options.BaseUrl} {request.Method}");
+
+            var response = client.Get(request);
+            Console.WriteLine(response.Content);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ Test.Project/TrelloTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using NUnit.Framework;
+using RestSharp;
+
+namespace Test.Project
+{
+    public class TrelloTest
+    {
+        private static RestClient _client;
+
+        [OneTimeSetUp]
+        public static void InializeRestClient() => _client = new RestClient("https://api.trello.com");
+
+        [Test]
+        public void CheckTrelloApi()
+        {
+            var request = new RestRequest();
+            Console.WriteLine($"{_client.Options.BaseUrl} {request.Method}");
+
+            var response = _client.Get(request);
+            Console.WriteLine(response.Content);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
